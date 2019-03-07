e139e763e89ef7280e260943cbbdd446998b73b6
Tania Ortiz-Rosales <tania_or58@yahoo.com>
2019-03-07 15:41:32 -0800

    Removed magic numbers from MainActivity.SectionsPagerAdapter, Added abstract super class FoodFragment, added AppetizerFragment, Refactored getPageTitle in MainActivity

--- app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
@@ -14,6 +14,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.lang.reflect.Array;
+
 public class MainActivity extends AppCompatActivity {
     private ShareActionProvider shareActionProvider;
 
@@ -61,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private class SectionsPagerAdapter extends FragmentPagerAdapter {
-        private static final int NUM_TABS = 4;
+        private static final int NUM_TABS = 5;
 
         public SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
@@ -69,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return 4;
+            return NUM_TABS;
         }
 
         @Override
@@ -83,23 +85,16 @@ public class MainActivity extends AppCompatActivity {
                     return new PastaFragment();
                 case 3:
                     return new StoresFragment();
+                case 4:
+                    return  new AppetizerFragment();
             }
             return null;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return getResources().getText(R.string.home_tab);
-                case 1:
-                    return getResources().getText(R.string.pizza_tab);
-                case 2:
-                    return getResources().getText(R.string.pasta_tab);
-                case 3:
-                    return getResources().getText(R.string.store_tab);
-            }
-            return null;
+            String[] item = getResources().getStringArray(R.array.tabs);
+                return (CharSequence) item[position];
         }
     }
 
