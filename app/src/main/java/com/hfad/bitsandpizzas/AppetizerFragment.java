ec5833dad4e1b65a6686ef5d21fb70a1075e0a6d
Ming Horn <minghaihorn@gmail.com>
2019-03-07 14:57:25 -0800

    Added appetizer tab fragment

---
+++ app/src/main/java/com/hfad/bitsandpizzas/AppetizerFragment.java
@@ -0,0 +1,7 @@
+package com.hfad.bitsandpizzas;
+
+public class AppetizerFragment extends ListArrayFragment {
+    public AppetizerFragment() {
+        super.setArrayId(R.array.appetizers);
+    }
+}
